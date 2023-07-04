af86050eb232483ecee71946ae5cbf98b4118415
AngeloDima <angelodimauro91@gmail.com>
2023-07-04 23:41:28 +0200

    add  section5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MenuComponent } from './menu/menu.component';
-import { DoveSiamoComponent } from './dove-siamo/dove-siamo.component';
-import { ContattaciComponent } from './contattaci/contattaci.component';
-import { GalleriaComponent } from './galleria/galleria.component';
+
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
 
 
 
-  //NavBar
-  { path: "menu", component: MenuComponent },
-  { path: "posizione", component: DoveSiamoComponent },
-  { path: "contattaci", component: ContattaciComponent },
-  { path: "galleria", component: GalleriaComponent }
+
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { Section1Component } from './home/section1/section1.component';
 
-import { MenuComponent } from './menu/menu.component';
-import { ContattaciComponent } from './contattaci/contattaci.component';
-import { DoveSiamoComponent } from './dove-siamo/dove-siamo.component';
-import { GalleriaComponent } from './galleria/galleria.component';
+
+import { Section1Component } from './home/section1/section1.component';
 import { Section2Component } from './home/section2/section2.component';
 import { Section3Component } from './home/section3/section3.component';
 import { Section4Component } from './home/section4/section4.component';
+import { Section5Component } from './home/section5/section5.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     Section1Component,
-    MenuComponent,
-    ContattaciComponent,
-    DoveSiamoComponent,
-    GalleriaComponent,
+
     Section2Component,
     Section3Component,
-    Section4Component
+    Section4Component,
+    Section5Component
   ],
   imports: [
     BrowserModule,
