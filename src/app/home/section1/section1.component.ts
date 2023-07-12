3978844bc29041b2138a447f0d16722349570d0f
AngeloDima <angelodimauro91@gmail.com>
2023-07-12 12:47:23 +0200

    lavagna img e blocco opaco

--- src/app/home/section1/section1.component.ts
+++ src/app/home/section1/section1.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section1',
   templateUrl: './section1.component.html',
   styleUrls: ['./section1.component.css']
 })
-export class Section1Component {
+export class Section1Component implements OnInit {
+  slideLavagna: any[] = [
+    {
+      slide: "https://demo.gridgum.com/bootstrap/pizza-store/PIZZA/images/slide01.jpg"
+    },
+    {
+      slide: "https://demo.gridgum.com/bootstrap/pizza-store/PIZZA/images/slide02.jpg"
+    },
+    {
+      slide: "https://demo.gridgum.com/bootstrap/pizza-store/PIZZA/images/slide03.jpg"
+    }
+  ];
 
+  currentSlideIndex = 0;
+
+  ngOnInit() {
+    setInterval(() => {
+      this.changeSlide();
+    }, 4000);
+  }
+
+  changeSlide() {
+    this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slideLavagna.length;
+  }
 }
